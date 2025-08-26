cd25239025d6be56a5b45f212759414a396300e2
Umang Maheshwari <umang.ladha15z@gmail.com>
2025-08-26 20:52:53 +0530

    Initial commit with workflow

---
+++ airflow_job_dag/gcp_dataproc_pyspark_dag.py
@@ -0,0 +1,102 @@
+from datetime import datetime, timedelta
+import uuid  # Import UUID for unique batch IDs
+from airflow import DAG
+from airflow.providers.google.cloud.operators.dataproc import (
+    DataprocCreateClusterOperator,
+    DataprocSubmitJobOperator,
+    DataprocDeleteClusterOperator,
+)
+from airflow.providers.google.cloud.sensors.gcs import GCSObjectExistenceSensor
+from airflow.models import Variable
+from airflow.utils.trigger_rule import TriggerRule
+
+# DAG default arguments
+default_args = {
+    "owner": "airflow",
+    "depends_on_past": False,
+    "retries": 1,
+    "retry_delay": timedelta(minutes=5),
+    "start_date": datetime.now(),
+}
+
+# Define the DAG
+with DAG(
+    dag_id="gcp_dataproc_pyspark_dag",
+    default_args=default_args,
+    schedule_interval="0 0 * * *",  # Trigger daily at midnight
+    catchup=False,
+) as dag:
+
+    # Fetch environment variables
+    gcs_bucket = Variable.get("gcs_bucket", default_var="airflow-projects-learning")
+    gcp_project = Variable.get("gcp_project", default_var="healthy-splice-469717-n5")
+
+    # # Task 1: File Sensor for GCS
+    file_sensor = GCSObjectExistenceSensor(
+        task_id="check_file_arrival",
+        bucket=gcs_bucket,
+        object=f"airflow-project-2/data/*.csv",
+        google_cloud_conn_id="google_cloud_default",  # GCP connection
+        timeout=43200,  # Timeout in seconds
+        poke_interval=300,  # Time between checks
+        mode="poke",  # Blocking mode
+    )
+
+    # Task 2: Submit PySpark job to Dataproc Serverless
+
+    REGION = "us-central1"
+    CLUSTER_NAME = "airflow-cluster-dataproc"
+    CLUSTER_CONFIG = {
+        "master_config": {
+            "num_instances": 1,
+            "machine_type_uri": "n1-standard-2",
+            "disk_config": {"boot_disk_type": "pd-standard", "boot_disk_size_gb": 32},
+        },
+        "worker_config": {
+            "num_instances": 2,
+            "machine_type_uri": "n1-standard-2",
+            "disk_config": {"boot_disk_type": "pd-standard", "boot_disk_size_gb": 32},
+        },
+        "software_config": {
+            "image_version": "2.3-debian12",
+        },
+    }
+
+    create_cluster = DataprocCreateClusterOperator(
+        task_id="create_cluster_dataproc",
+        project_id=gcp_project,
+        cluster_config=CLUSTER_CONFIG,
+        region=REGION,
+        cluster_name=CLUSTER_NAME,
+    )
+
+    PYSPARK_JOB = {
+        "reference": {"project_id": gcp_project},
+        "placement": {"cluster_name": CLUSTER_NAME},
+        "pyspark_job": {
+            "main_python_file_uri": f"gs://{gcs_bucket}/airflow-project-2/spark-job/dataproc_pyspark_job.py",
+            "args": [
+                f"--gcp_project={gcp_project}",
+                f"--gcs_bucket={gcs_bucket}"
+            ],
+        },
+    }
+
+    pyspark_job = DataprocSubmitJobOperator(
+        task_id="submit_pyspark_job_on_dataproc",
+        job=PYSPARK_JOB,
+        region=REGION,
+        project_id=gcp_project,
+    )
+
+    delete_cluster = DataprocDeleteClusterOperator(
+        task_id="delete_cluster_dataproc",
+        cluster_name=CLUSTER_NAME,
+        region=REGION,
+        project_id=gcp_project,
+        # Ensures cluster deletion runs regardless of previous task success or failure
+        trigger_rule=TriggerRule.ALL_DONE,
+    )
+
+    # Task Dependencies
+    file_sensor >> pyspark_job >> delete_cluster
---
+++ spark_job/dataproc_pyspark_job.py
@@ -0,0 +1,86 @@
+import argparse
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, count, avg, when, lit, expr
+import logging
+import sys
+
+# Initialize Logging
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
+logger = logging.getLogger(__name__)
+
+def main(extension, gcp_project, gcs_bucket):
+    spark = None
+    try:
+        # Initialize SparkSession
+        spark = SparkSession.builder \
+            .appName("DataProcessingandHiveTable") \
+            .config("spark.sql.catalogImplementation", "hive") \
+            .enableHiveSupport() \
+            .getOrCreate()
+
+        logger.info("Spark session initialized.")
+
+        # Resolve GCS path based on the environment
+        input_path = f"gs://{gcs_bucket}/airflow-project-2/data"
+        logger.info(f"Input path resolved: {input_path}")
+
+        college_data = None
+        # Read the data from GCS
+        college_data = spark.read.csv(input_path, header=True, inferSchema=True)
+
+        if college_data is None:
+            logger.error("Failed to load data. Please check the input path and file extension.")
+            sys.exit(1)
+        
+        logger.info("Data read from GCS.")
+
+        # Data transformations
+        logger.info("Starting data transformations.")
+
+        sql_query = """ CREATE DATABASE IF NOT EXISTS airflow_assignment_1"""
+        spark.sql(sql_query)
+
+        #1 Total Placements by College IDs
+
+        total_placements = college_data.groupBy("college_id").agg(
+            count(when(col("Placement") == "Yes", 1)).alias("total_placements"), 
+            count(when(col("Placement") == "No", 1)).alias("total_non_placements"),
+            col("total_placements") / (col("total_placements") + col("total_non_placements")).alias("placement_rate")
+        )
+
+        logger.info("Data transformations completed.")
+
+        # Write transformed data to Hive
+        
+        total_placements.write.format("hive").mode("append").saveAsTable("airflow_assignment_1.total_placements")
+
+        logger.info("Data written to Hive successfully.")
+
+    except Exception as e:
+        logger.error(f"An error occurred: {e}")
+        sys.exit(1)
+
+    finally:
+        # Stop Spark session
+        if spark is not None:
+            spark.stop()
+            logger.info("Spark session stopped.")
+
+if __name__ == "__main__":
+    # Parse command-line arguments
+    parser = argparse.ArgumentParser(description="Process Data and write to Hive Table.")
+    parser.add_argument("--extension", required=True, help="File extension (e.g., csv, json)")
+    parser.add_argument("--gcp_project", required=True, help="GCP project ID")
+    parser.add_argument("--gcs_bucket", required=True, help="GCS bucket name")
+
+    args = parser.parse_args()
+
+    # Call the main function with parsed arguments
+    main(
+        extension=args.extension,
+        gcp_project=args.gcp_project,
+        gcs_bucket=args.gcs_bucket
+    )
